adbbee5a20b28e06f6ed30963ca08a7073f5bf65
welcomeqb <38384623+welcomeqb@users.noreply.github.com>
2018-05-12 21:10:04 -0400

    2nd commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  title = 'QB Home';
+  itemCount: number = 4;
+  btnText : string = "add one";
+  goalText:string = "initial goal";
+  goalList = [];
+
+  constructor() { }
+
+  ngOnInit() {
+    this.itemCount =this.goalList.length;
+  }
+
+  addGoal(){
+    
+    this.goalList.push(this.goalText);
+    this.goalText="";
+    this.itemCount= this.goalList.length;
+   
+
+  }
+
+  removeItem(i){
+
+    this.goalList.splice(i,1);
+    this.itemCount = this.goalList.length;
+  }
+
+}
